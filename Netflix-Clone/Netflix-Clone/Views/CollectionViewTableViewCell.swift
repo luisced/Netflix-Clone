fc286204b011904e33028d14d4007314f220ab4d
luisced <luis.cedillo.maldonado@gmail.com>
2023-06-12 00:14:12 -0600

    created project

---
+++ Netflix-Clone/Netflix-Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix-Clone
+//
+//  Created by Luis Cedillo M on 12/06/23.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
